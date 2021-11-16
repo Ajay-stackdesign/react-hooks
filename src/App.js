6027b7fff46b1417cbb2f73b97262ddd7c464d22
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-16 22:12:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from "react"
+import React, { createContext, useState } from "react"
 import Counter from "./Counter";
 import Reducer1 from "./Reducer1";
+import Effect from "./Effect";
+import Refhook from "./Refhook";
+// import UseLayoutEffect from "./UseLayoutEffect";
+import LayoutEffect from "./LayoutEffect";
+import Imperativehandle from "./Imperatie/ImperativeHandle";
+import Context from "./context/Context1";
+
+const UserContext = createContext()
 
 function App() {
+
+  const [user, setUser] = useState("ajay");
+
+  const changeUser = (e) => {
+    setUser(e.target.value)
+  }
+
   return (
     <div className="App">
       <Counter />
       <Reducer1 />
+      <Effect />
+      <Refhook />
+      <LayoutEffect />
+      <Imperativehandle />
+      {/* <UserContext.Provider value={user}> */}
+      <UserContext.Provider value={"ajay"}>
+      {/* <h1>{`Hello ${user}`}</h1> */}
+      <Context />
+      </UserContext.Provider>
+      {/* <UserContext.Provider value={user}> */}
+      {/* <Context user = {user}/> */}
+      {/* <h1>{`hello ${user}`}</h1> */}
+      {/* </UserContext.Provider> */}
     </div>
   );
 }
---
+++ src/Context.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Context1 from "./Context1";
+
+
+const Context = () => {
+    
+
+    return(
+        <div>
+            <Context1 />
+        </div>
+    )
+}
+
+export default Context
