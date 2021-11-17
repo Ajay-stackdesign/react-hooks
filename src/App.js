471e0a7d412f7956179e82cbdf830c3834a022ef
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-17 21:15:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react"
+import React, { createContext, useEffect, useState }  from "react"
 import Counter from "./Counter";
 import Reducer1 from "./Reducer1";
 import Effect from "./Effect";
@@ -6,17 +6,17 @@ import Refhook from "./Refhook";
 // import UseLayoutEffect from "./UseLayoutEffect";
 import LayoutEffect from "./LayoutEffect";
 import Imperativehandle from "./Imperatie/ImperativeHandle";
-import Context from "./context/Context1";
+import Context from "./Context";
 
-const UserContext = createContext()
+
+const UserConnect = createContext()
 
 function App() {
-
-  const [user, setUser] = useState("ajay");
-
-  const changeUser = (e) => {
-    setUser(e.target.value)
-  }
+  const [user, setUser] = useState("ajay") ;
+  
+  useEffect(() => {
+    setUser("let buil it")
+  },[])
 
   return (
     <div className="App">
@@ -26,17 +26,12 @@ function App() {
       <Refhook />
       <LayoutEffect />
       <Imperativehandle />
-      {/* <UserContext.Provider value={user}> */}
-      <UserContext.Provider value={"ajay"}>
-      {/* <h1>{`Hello ${user}`}</h1> */}
-      <Context />
-      </UserContext.Provider>
-      {/* <UserContext.Provider value={user}> */}
-      {/* <Context user = {user}/> */}
-      {/* <h1>{`hello ${user}`}</h1> */}
-      {/* </UserContext.Provider> */}
+      <UserConnect.Provider value={user}>
+        <Context />
+      </UserConnect.Provider>
     </div>
   );
 }
 
 export default App;
+export { UserConnect }
--- src/Reducer1.js
+++ src/Reducer1.js
@@ -1,24 +1,64 @@
-import React, { useReducer } from "react"
+// import React, { useReducer } from "react"
 
-const reducer = ((state,action) => {
+// const reducer = ((state, action) => {
+//     switch (action.type) {
+//         case "INCREMENT":
+//             return { count: state.count + 1, showText: state.showText }
+//         case "toggleShowText":
+//             return { count: state.count, showText: !state.showText }
+//         default:
+//             return state;
+//     }
+
+
+// const Reducer1 = () => {
+//     const [state, dispatch] = useReducer(reducer, { count: 0, showText: true })
+
+
+//     return (
+//         <div>
+//             <h1>{state.count}</h1>
+//             {/* <button onClick={() =>
+//                 dispatch({ type: "INCREMENT" })
+//                 dispatch({type: "toggleShowText"})}
+//                 >click me
+//                 </button> */}
+//             <button
+//              onclick={() => {
+//                 dispatch({type: "INCREMENT"});
+//                 dispatch({type: "toggleShowText"});
+//             }}>click me</button>
+//             { state.showText && <h1>this is a text!</h1> }
+//         </div >
+//     )
+// }
+
+// export default Reducer1
+
+import React, { useReducer } from "react";
+
+const reducer = (state,action) => {
     switch(action.type){
         case "INCREMENT":
-            return{count: state.count + 1 ,showText: state.showText}
+            return{ count: state.count + 1, showText: state.showText};
+        case "toggleShowText":
+            return{ count: state.count, showText: !state.showText};
         default:
             return state;
     }
-})
-
+}
 const Reducer1 = () => {
-    const [state, dispatch] = useReducer(reducer,{count: 0, showText: true } )
-
-
+    const [state, dispatch] = useReducer(reducer, { count: 0, showText: true})
+    
     return(
         <div>
-           <h1>{state.count}</h1>
-           <button onClick={() => dispatch({type: "INCREMENT"})}>click me</button>
-           {state.showText}<h1>this is a text!</h1>
-        </div>
+            <h1>{ state.count}</h1>
+            <button onClick={() => {
+                dispatch({ type: "INCREMENT"});
+                dispatch({type: "toggleShowText"})
+            }}>click me</button>
+            { state.showText && <p>this the text</p>}
+        </div>    
     )
 }
 
