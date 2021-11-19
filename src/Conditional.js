6c9b2a2cfe86d1b7ba509c27cb04a361c197c5c4
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-19 20:40:17 +0530

    changes

---
+++ src/Conditional.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Conditional = (props) => {
+    
+    return(
+        <div>
+           <li> i am a { props.brand1 } </li> 
+        </div>
+    )
+}
+
+export default Conditional
