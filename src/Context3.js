d5410664fd9ebc68f48dee1c4703b2fbbb40a837
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-11-26 23:31:28 +0530

    changes

--- src/Context3.js
+++ src/Context3.js
@@ -2,12 +2,12 @@ import React, { useContext } from "react"
 import { UserConnect } from "./App"
 
 const Context3 = () => {
+
     const user = useContext(UserConnect)
     
     return(
         <div>
             <h2>hello duniya {user}</h2>
-            
         </div>
     )
 }
